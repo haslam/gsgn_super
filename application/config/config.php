787939d961f424a76922eb671d808e8abfb0fd0b
haslam <haslam414@gmail.com>
2015-12-15 14:42:32 +0100

    Basic CI config and htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,8 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+//$config['base_url'] = 'super.goodschoolsguidenigeria.com';
+$config['base_url'] = 'localhost:8080/gsgn-super';
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -232,7 +233,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'Yh652GGS9S8JJF7HhySo0yGSHiyUB9827HsGiO';
 
 /*
 |--------------------------------------------------------------------------
@@ -252,12 +253,12 @@ $config['encryption_key'] = '';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= 'gsgn_session';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
-$config['sess_table_name']		= 'ci_sessions';
+$config['sess_table_name']		= 'gsgn_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
